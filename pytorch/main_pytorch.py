0abb8a88cc749d87ca01287059341f943b4b9783
agnes-yang <agnes.eyre@gmail.com>
2022-02-24 19:49:02 +0800

    add some notes

--- pytorch/main_pytorch.py
+++ pytorch/main_pytorch.py
@@ -19,7 +19,7 @@ import argparse
 DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 result = []
 
-
+# 用来获取输入参数
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--nepoch', type=int, default=50)
@@ -33,49 +33,58 @@ def get_args():
     return args
 
 
+# 训练函数，加上测试的功能，每一轮训练都加测试一遍
 def train(model, optimizer, train_loader, test_loader):
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss()  # 定义损失函数，为交叉熵损失
 
-    for e in range(args.nepoch):
-        model.train()
-        correct, total_loss = 0, 0
+    for e in range(args.nepoch):  # 对每一个 epoch 的循环
+        model.train()             # 定义模型在训练模式
+        correct, total_loss = 0, 0   #
         total = 0
-        for sample, target in train_loader:
+        for sample, target in train_loader:   # 对于训练集中的每一个 batch size 的样本和对应标签
             sample, target = sample.to(
-                DEVICE).float(), target.to(DEVICE).long()
-            sample = sample.view(-1, 9, 1, 128)
-            output = model(sample)
-            loss = criterion(output, target)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            total_loss += loss.item()
-            _, predicted = torch.max(output.data, 1)
-            total += target.size(0)
-            correct += (predicted == target).sum()
-        acc_train = float(correct) * 100.0 / len(train_loader.dataset)
+                DEVICE).float(), target.to(DEVICE).long()  # 将数据放到 gpu 上
+            sample = sample.view(-1, 9, 1, 128)            #
+            output = model(sample)                         # 计算输入模型，计算输出
+            loss = criterion(output, target)               # 通过输出与真实标签计算 loss
+            optimizer.zero_grad()                          # 将优化器梯度置零
+            loss.backward()                                # 将 loss 反向传播
+            optimizer.step()                               # 优化器开始优化
+            total_loss += loss.item()                      # 叠加 loss
+            _, predicted = torch.max(output.data, 1)       # 取 输出中最大的概率 对应的标签 做为结果
+            total += target.size(0)                        # total 累加每个 batch size 参与训练的样本数
+            correct += (predicted == target).sum()         # correct 累加分类正确的样本数
+        acc_train = float(correct) * 100.0 / len(train_loader.dataset)  # 计算准确率
 
         # Testing
         acc_test = valid(model, test_loader)
-        print(f'Epoch: [{e}/{args.nepoch}], loss:{total_loss / len(train_loader):.4f}, train_acc: {acc_train:.2f}, test_acc: {float(correct) * 100 / total:.2f}')
+        # 输出每轮轮次，loss 为叠加每个 batch 的 loss 除以 batch 的总数，训练 accuracy 为 正确分类样本数 除以 总样本数，
+        print(f'Epoch: [{e}/{args.nepoch}], /'
+              f'total_loss:{total_loss:.4f}, len(train_loader):{len(train_loader)}, '
+              f'loss:{total_loss / len(train_loader):.4f}, /'
+              f'correct:{float(correct)}, len(train_loader.dataset): {len(train_loader.dataset)}, '
+              f'train_acc: {acc_train:.2f}, /'
+              f'correct:{float(correct)}, total: {total}, '
+              f'test_acc: {float(correct) * 100 / total:.2f}')
         result.append([acc_train, acc_test])
         result_np = np.array(result, dtype=float)
-        np.savetxt('result.csv', result_np, fmt='%.2f', delimiter=',')
+        # np.savetxt('result.csv', result_np, fmt='%.2f', delimiter=',')
 
 
+# 在测试集上测试结果
 def valid(model, test_loader):
     model.eval()
-    with torch.no_grad():
+    with torch.no_grad():    # 声明不计算梯度
         correct, total = 0, 0
-        for sample, target in test_loader:
+        for sample, target in test_loader:                     # 对于测试集中的每一个 batch size 的样本和对应标签
             sample, target = sample.to(
-                DEVICE).float(), target.to(DEVICE).long()
-            sample = sample.view(-1, 9, 1, 128)
-            output = model(sample)
-            _, predicted = torch.max(output.data, 1)
-            total += target.size(0)
-            correct += (predicted == target).sum()
-    acc_test = float(correct) * 100 / total
+                DEVICE).float(), target.to(DEVICE).long()      # 搬移数据到 gpu 上
+            sample = sample.view(-1, 9, 1, 128)                #
+            output = model(sample)                             # 计算输出
+            _, predicted = torch.max(output.data, 1)           # 取 输出中最大的概率 对应的标签 做为结果
+            total += target.size(0)                            # total 累加每个 batch size 参与测试的样本数
+            correct += (predicted == target).sum()             # correct 累加分类正确的样本数
+    acc_test = float(correct) * 100 / total                    # 计算准确率
     return acc_test
 
 
--- pytorch/network.py
+++ pytorch/network.py
@@ -3,6 +3,18 @@
     Created on 10:41 2018/11/10 
     @author: Jindong Wang
 """
+
+"""
+神经网络的记录:
+输入为 N x channel x height x weight, 每一条记录包含 9 个数据当做 channel，height做为1，width为数据长度
+第一层： 输入 32 channel，kernel size 卷积核大小为 1 x 9，stride 步长为默认值 1，得 9 x 1 x 128 -> 32 x 1 x 120
+最大池化层： kernel size 为 1 x 2，stride 为 2，32 x 1 x 120 -> 32 x 1 x 60
+第二层：input 32 channel，output 64 channel，kernel 为 1 x 9，stride 为 1，32 x 1 x 60 -> 64 x 1 x 52
+最大池化层： kernel size 为 1 x 2，stride 为 2，64 x 1 x 62 -> 64 x 1 x 26
+合为一维向量，64 * 26
+三个全连接层： 64 * 26 -> 1000 -> 5000 -> 6
+"""
+
 import torch.nn as nn
 import torch.nn.functional as F
 
